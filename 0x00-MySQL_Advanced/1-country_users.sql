708e90bfdb0274e271dcd9f9568fb05484861977
max <maxwelgithumbi@gmail.com>
2022-04-20 13:55:12 +0300

    in and out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,7 @@
+-- script that creates table users
+create table if not exists `users` (
+    `id` int not null primary key auto_increment,
+    `email` varchar(255) not null unique,
+    `name` varchar(255),
+    `country` ENUM('US', 'CO', 'TN') not null
+);
